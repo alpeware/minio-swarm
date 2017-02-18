71ba3b932ae5a1e8f21439bd189aec8e29e9b30c
simonpure <simon.l+git@gmail.com>
2017-02-17 18:24:32 -0800

    init swarm

--- minio-swarm.sh
+++ minio-swarm.sh
@@ -2,6 +2,8 @@
 
 REPLICAS=${1:-4}
 
+docker swarm init --advertise-addr=$(ifconfig eth0| grep 'inet addr:'| cut -d: -f2 | awk '{ print $1}')
+
 openssl rand -hex 10 > .minio_access_key
 docker secret create MINIO_ACCESS_KEY .minio_access_key
 
